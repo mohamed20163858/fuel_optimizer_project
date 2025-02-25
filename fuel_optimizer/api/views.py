ca2c4c740c696d4cff13cd571e7346052b117764
Mohamed Saleh <mohamed20163858@gmail.com>
2025-02-26 01:18:55 +0200

    Create simple endpoint to query fuel prices data from db

--- fuel_optimizer/api/views.py
+++ fuel_optimizer/api/views.py
@@ -1,6 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
+from .models import FuelPrice
+from .serializers import FuelPriceSerializer
+
+class FuelPriceListView(generics.ListAPIView):
+    queryset = FuelPrice.objects.all()
+    serializer_class = FuelPriceSerializer
 
 class RouteFuelView(APIView):
     def post(self, request, *args, **kwargs):
